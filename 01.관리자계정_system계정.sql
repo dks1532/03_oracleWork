83ab6d194c6d04078729f2acef1afcc2118044b9
dks1532 <127491851+dks1532@users.noreply.github.com>
2023-04-11 10:05:50 +0900

    Initial commit

---
+++ 01.관리자계정_system계정.sql
@@ -0,0 +1,27 @@
+-- 한줄 주석 : 단축키 ctrl + /
+/*
+    여러줄 주석 : 단축키 alt + shift + c 
+*/
+
+-- 사용자 생성 : 실행시 단축키 ctrl + enter
+create user c##test2 identified by 1234;
+
+-- 이를 회피하는 방법 (c## 안붙여도 계정 생성 가능)
+alter session set "_oracle_script" = true;
+
+-- 앞으로 테이블을 생성하고 사용하려면 아래 3가지를 해야함
+-- 1. kh 사용자 생성
+create user kh identified by 1234;
+
+-- 2. 권한부여
+grant connect, resource to kh;
+
+-- 3. 테이블스페이스 할당
+-- alter user kh quota 30M on users;   -- 명시
+alter user kh default tablespace users quota unlimited on users; -- 제한을 두지않고
+
+-- 사용자 삭제
+drop user c##test2;
+
+-- 테이블이 존재할 경우 사용자 삭제
+drop user c##test2 cascade;
